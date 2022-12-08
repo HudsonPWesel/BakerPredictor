a1ecd2409fc16232e019694250b3f5fe62d0a66d
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2022-12-07 20:11:08 -0500

    Basic Functionality

--- Baker.py
+++ Baker.py
@@ -1,16 +1,22 @@
+"""The baker object which has all information about each baker"""
+
+
 class Baker:
     def __init__(self, weight, name, char):
-        self.name = name
-        self.weight = weight
-        self.currentScore = 0
+        self.name = name            # Name of baker ("Baker #")
+        self.weight = weight        # Weight of each baker (Constant)
+        self.currentScore = 0       # Score for each round
+        # Amount of wins a baker has (trial wins not round wins)
         self.winCount = 0
-        self.win_percentage = 0
+        self.win_percentage = 0     # Percentage of times the baker wins
         self.weeks_eliminated = {"Week 1": 0, "Week 2": 0, "Week 3": 0, "Week 4": 0, "Week 5": 0,
                                  "Week 6": 0, "Week 7": 0, "Week 8": 0, "Week 9": 0, "Week 10": 0, "Week 11": 0, "Week 12": 0}
-        self.char = char
-        self.num_final_rounds = 0
-        self.num_final_three = 0
+        self.char = char            # char which is unique to the baker
+        self.num_final_rounds = 0   # Amount of rounds in final (top 3)
+        self.num_final_five = 0     # Amount of rounds in top 5
+        # The rank for each round (3 rounds per removal)
         self.rank = 0
+        # The ranks for each round (Used to see of baker should be removed)
         self.cumulative_ranks = []
-
-
+        # Cumulative score per ROUND (every 3 challenges)
+        self.cumulative_score = 0
--- BakerEngine.py
+++ BakerEngine.py
@@ -1,77 +1,122 @@
-import numpy as np
-import pandas as pd
+
 from Baker import Baker
+# import pandas as pd
 # from scipy.stats import norm
 # import matplotlib.pyplot as plt
 
-
 # For Data Formating
+import pandas as pd
+import numpy as np
+
+# Global Variables
 baker_wins = {"Baker 1": 0, "Baker 2": 0, "Baker 3": 0, "Baker 4": 0,
               "Baker 5": 0, "Baker 6": 0, "Baker 7": 0, "Baker 8": 0,
               "Baker 9": 0, "Baker 10": 0, "Baker 11": 0, "Baker 12": 0}
+char_arr = []   # Chars for each round
+baker_list = []  # List of baker objects
 
-BAKER_RANKS = (14, 12, 10, 10, 10, 10, 10, 10, 10, 10, 8, 6)
-CHARS = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L')
-data = pd.DataFrame()
-char_arr = []
-baker_list = []
 
-def fill_char_arr(baker_list_copy: list):
+# CONSTANTS
+BAKER_RANKS = (14, 12, 10, 10, 10, 10, 10, 10, 10, 10, 8,
+               6)         # Weights if each baker in order
+CHARS = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+         'K', 'L')  # Bakers corresponding letter in order
+DATA = pd.DataFrame()
+
+
+def fill_char_arr():
+    """Fill list of chars determined by the weight of each baker"""
     char_arr.clear()
-    rank = 1
     for baker in baker_list_copy:
         for i in range(baker.weight):
             char_arr.append(baker.char)
 
 
-def elim_char(baker_list_copy, current_rank):
+def set_baker_scores():
+    """Find winner of each challenge (Three for each baker removal)"""
     global char_arr
-    chosen_num = np.random.randint(0, len(char_arr))
-    chosen_char = char_arr[chosen_num]
+    # Rank is place per challenge (1st, 2nd, 3rd, etc. places)
 
-    char_arr = [current_char for current_char in char_arr if current_char != char_arr[chosen_num]]
-    assign_rank(chosen_char, current_rank)
-def assign_rank(chosen_char, current_rank):
-    for baker in baker_list:
+    # Find range and a char (Each baker has a char that corresponds to it)
+    for i in range(0, 13):
+        chosen_num = 0
+        if(len(char_arr) != 0):
+            chosen_num = np.random.randint(0, len(char_arr))
+            chosen_char = char_arr[chosen_num]
+
+        # Remove all occurances of char from the list of baker chars (remove baker)
+        char_arr = [
+            current_char for current_char in char_arr if current_char != char_arr[chosen_num]]
+        assign_rank(chosen_char, i)
+
+
+def assign_rank(chosen_char, current_rank,):
+    """Assign a rank to each baker in the baker Object"""
+    for baker in baker_list_copy:
+
         if baker.char == chosen_char:
             baker.cumulative_ranks.append(current_rank)
 
 
-
-
-
-
-def find_range(baker_list: list) -> int:
+def find_range() -> int:
+    """Find the range of the bakers by adding up all the bakers weigt"""
     total = 0
-    for baker in baker_list:
+    for baker in baker_list_copy:
         total += baker.weight
     return total - 1
 
 
-def simulate_round(baker_list: list):
-    currentWeek = 0
+def calc_scores(scores):
+    for baker in baker_list_copy:
+        for score in baker.cumulative_ranks:
+            baker.cumulative_score += score
+        scores.append(baker.cumulative_score)
 
+
+def eliminate_baker(current_week, scores):
+    # TODO: Problem: After the baker is eliminated, we cant use that baker to display in DF
+    for baker in baker_list_copy:
+        if baker.cumulative_score == max(scores):
+            baker_list_copy.remove(baker)
+            baker.weeks_eliminated["Week " + (str(current_week + 1))] += 1
+        print(baker.weeks_eliminated)
+
+    print("Remaining Bakers" + (str(len(baker_list_copy))))
+
+
+def reset_baker_scores():
+    for baker in baker_list_copy:
+        baker.cumulative_ranks = []
+
+
+def simulate():
+    scores = []
+
+    """Simulate a single trial"""
+    global baker_list_copy
     baker_list_copy = baker_list.copy()
-    for challenge in range (3):
-        fill_char_arr(baker_list_copy)
+
+    # RUNS UP TO FINAL 3
+    for week in range(9):
+        # Runs three times to simulate the three challenges each week
+        reset_baker_scores()  # Reset cumulative ranks
+        for challenge in range(3):
+            # Reset arr for each challenge
+            fill_char_arr()  # Fills with 120 chars based on weight
+            set_baker_scores()
+        calc_scores(scores)  # Calculate cumulative score
+        # Eliminate baker with max score of scores
+        eliminate_baker(week, scores)
+        scores = []
 
 
-        for i in range (1, 13):
-
-            elim_char(baker_list_copy, i)
-    # print(baker_list[1].cumulative_ranks)
-
-       # print(char_arr)
-       # print(chosen_num)
-
-
-
-
-#         Iterate through every baker
-
+def display_DataFrame():
+    """Display the final data for each simulation"""
+    print(pd.DataFrame({"Bakers": list(baker_wins.keys())}))
 
 
 def main():
+    """Start of program...!"""
     try:
         i: int = int(input("Please enter the amount of trials to run: "))
     except:
@@ -80,40 +125,27 @@ def main():
 
 
 def run(epochs: int):
+    """Run the simulation a set amount of times"""
 
+    # Initialze Baker Objects
     for i in range(12):
-        baker_list.append(Baker(BAKER_RANKS[i], ("Baker " + str(i + 1), ), CHARS[i]))
+        baker_list.append(
+            Baker(BAKER_RANKS[i], ("Baker " + str(i + 1), ), CHARS[i]))
+
+    # Run each Simulation
     for i in range(1, epochs+1):
         # % Program Finished
         print(str(i) + " | " + "{:.2f}".format((i/epochs)*100) + "%")
-        simulate_round(baker_list)
+        # Run one simulation (Saves output globally)
+        simulate()
+
+    # Display data for all the simulations
+
+    # display_DataFrame()
+
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # from scipy.stats import norm
